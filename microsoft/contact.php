d75630f0eb818c0b899e9f8e2889c043dce634ba
Leandro Pereira <40795185+leandrojfpereira@users.noreply.github.com>
2018-12-23 16:19:32 +0000

    Alteração Menu
    Criei "Menu.php" introduzi dentro das outras Páginas

--- microsoft/contact.php
+++ microsoft/contact.php
@@ -38,37 +38,7 @@
 </head>
 
 <body>
-
-	<!-- Start Header Area -->
-	<header id="header">
-		<div class="container">
-			<div class="row align-items-center justify-content-between d-flex">
-				<div id="logo">
-					<a href="index.html"><img src="img/logo.png" alt="" title="" /></a>
-				</div>
-				<nav id="nav-menu-container">
-					<ul class="nav-menu">
-						<li><a href="index.html">Home</a></li>
-						<li><a href="about.html">About</a></li>
-						<li class="menu-active"><a href="courses.html">Courses</a></li>
-						<li class="menu-has-children"><a href="">Pages</a>
-							<ul>
-								<li><a href="elements.html">Elements</a></li>
-							</ul>
-						</li>
-						<li class="menu-has-children"><a href="">Blog</a>
-							<ul>
-								<li><a href="blog-home.html">Blog Home</a></li>
-								<li><a href="blog-single.html">Blog Details</a></li>
-							</ul>
-						</li>
-						<li><a href="contact.html">Contact</a></li>
-					</ul>
-				</nav><!-- #nav-menu-container -->
-			</div>
-		</div>
-	</header>
-	<!-- End Header Area -->
+	<?php  require_once "menu.php"; ?>
 
 
 	<!-- Start Banner Area -->
