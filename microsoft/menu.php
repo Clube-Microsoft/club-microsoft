a81e5601f58aa0ccbf37f49c75459c29d1cdfe57
Leandro Pereira <40795185+leandrojfpereira@users.noreply.github.com>
2018-12-23 16:41:34 +0000

    Alteração do footer.php e menu.php
    Retirar o "Echo"

--- microsoft/menu.php
+++ microsoft/menu.php
@@ -1,32 +1,29 @@
-<?php
+<header id="header">
+		<div class="container">
+			<div class="row align-items-center justify-content-between d-flex">
+				<div id="logo">
+					<a href="index.php"><img src="img/logo.png" alt="" title="" /></a>
+				</div>
+				<nav id="nav-menu-container">
+					<ul class="nav-menu">
+						<li class="menu-active"><a href="index.php">Inicio</a></li>
+						<li><a href="about.php">About</a></li>
+						<li><a href="courses.php">Courses</a></li>
+						<li class="menu-has-children"><a href="">Pages</a>
+							<ul>
+								<li><a href="elements.php">Elements</a></li>
+							</ul>
+						</li>
+						<li class="menu-has-children"><a href="">Blog</a>
+							<ul>
+								<li><a href="blog-home.php">Blog Home</a></li>
+								<li><a href="blog-single.php">Blog Details</a></li>
+							</ul>
+						</li>
+						<li><a href="contact.php">Contact</a></li>
+					</ul>
+				</nav><!-- #nav-menu-container -->
+			</div>
+		</div>
+	</header>
 
-echo"<header id='header'>".
-		"<div class='container'>".
-			"<div class='row align-items-center justify-content-between d-flex'>".
-				"<div id='logo'>".
-					"<a href='index.php'><img src='img/logo.png' alt='' title='' /></a>".
-				"</div>".
-				"<nav id='nav-menu-container'>".
-					"<ul class='nav-menu'>".
-						"<li class='menu-active'><a href='index.php'>Inicio</a></li>".
-						"<li><a href='about.php'>About</a></li>".
-						"<li><a href='courses.php'>Courses</a></li>".
-						"<li class='menu-has-children'><a href=''>Pages</a>".
-							"<ul>".
-								"<li><a href='elements.php'>Elements</a></li>".
-							"</ul>".
-						"</li>".
-						"<li class='menu-has-children'><a href=''>Blog</a>".
-							"<ul>".
-								"<li><a href='blog-home.php'>Blog Home</a></li>".
-								"<li><a href='blog-single.php'>Blog Details</a></li>".
-							"</ul>".
-						"</li>".
-						"<li><a href='contact.php'>Contact</a></li>".
-					"</ul>".
-				"</nav>".
-			"</div>".
-		"</div>".
-	"</header>"
-
-?>
