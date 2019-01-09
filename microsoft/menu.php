06ca98ac01a161014cc682354fc05b9c63b17d51
Leandro Pereira <40795185+leandrojfpereira@users.noreply.github.com>
2019-01-09 16:36:57 +0000

    Update menu.php

--- microsoft/menu.php
+++ microsoft/menu.php
@@ -2,12 +2,11 @@
 	<div class="container">
 		<div class="row align-items-center justify-content-between d-flex">
 			<div id="logo">
-				<a href="index.php"><img src="img/Logo/LogoName.png" alt="" title="" width="350px" /></a>
+				<a href="index.php"><img src="img/Logo/LogoName.png" alt="" title="" class="img_menu"/></a>
 			</div>
 			<nav id="nav-menu-container">
 				<ul class="nav-menu">
 					<li><a href="index.php">Início</a></li>
-					<!--<li><a href="about.php">About</a></li>-->
 					<li><a href="courses.php">Cursos</a></li>
 					<li class="menu-has-children">
 						<a>Serviços</a>
@@ -18,21 +17,23 @@
 							<li><a href="#">Ajuda em Tempo Real</a></li>
 						</ul>
 					</li>
-					<!--<li class="menu-has-children"><a href="">Pages</a>
-						<ul>
-							<li><a href="elements.php">Elements</a></li>
-						</ul>
-					</li>
-					<li class="menu-has-children"><a href="">Blog</a>
-						<ul>
-							<li><a href="blog-home.php">Blog Home</a></li>
-							<li><a href="blog-single.php">Blog Details</a></li>
-						</ul>
-					</li>-->
 					<li><a href="contact.php">Contacto</a></li>
 				</ul>
 			</nav><!-- #nav-menu-container -->
 		</div>
 	</div>
 </header>
+
+
+<style>
+    .img_menu{
+    	width:350px;
+    }
+    
+	@media only screen and (max-width: 881px) {
+        .img_menu{
+        	width:150px!important;
+        }
+    }
+</style>
 
