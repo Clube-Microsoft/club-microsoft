4ccc9ad95dd97ab187dfb009199cee3a70aab3f6
Leandro Pereira <40795185+leandrojfpereira@users.noreply.github.com>
2019-01-17 22:06:07 +0000

    Update blog-single-mos.php

--- microsoft/blog-single-mos.php
+++ microsoft/blog-single-mos.php
@@ -1,110 +1,96 @@
 <!DOCTYPE html>
 <html lang="zxx" class="no-js">
-
-<head>
-<?php  require_once "links.php"; ?>
-	<!-- Site Title -->
-	<title>Exame MOS - Evento</title>
-
-	
-</head>
-
-<body>
-	<?php  require_once "menu.php"; ?>
-
-
-	<!-- Start Banner Area -->
-	<section class="banner-area relative">
-		<div class="container">
-			<div class="row d-flex align-items-center justify-content-center">
-				<div class="about-content col-lg-12">
-					<h1 class="text-white">
-						Exame MOS
-					</h1>
-					<p>Este exame pode ser feito por qualquer aluno da EPB - Escola Profissional de Braga sem qualquer custo, podendo ser fornecido uma certificação em MOS (Microsoft Office Specialist).</p>
-					<div class="link-nav">
-						<span class="box">
-							<a href="index.php">Home </a>
-							<i class="lnr lnr-arrow-right"></i>
-							<a href="blog-home.php">Blog</a>
-							<i class="lnr lnr-arrow-right"></i>
-							<a href="blog-single-mos.php">Exame MOS</a>
-						</span>
-					</div>
-				</div>
-			</div>
-		</div>
-		<div class="rocket-img go-bottom">
-			<img src="img/rocket.png" alt="" style="margin-bottom:-8px;">
-		</div>
-	</section>
-	<!-- End Banner Area -->
-
-	<!-- Start post-content Area -->
-	<section class="post-content-area single-post-area">
-		<div class="container">
-			<div class="row">
-				<div class="col-lg-12 posts-list">
-					<div class="single-post row">
-						<div class="col-lg-12">
-							<div class="feature-img col-12 col-lg-8" style="float: left;">
-								<img class="img-fluid img-blog-post-dentro" src="img/blog/mos_blog.png" alt="">
-							</div>
-							<div class="col-12 col-lg-4" style="float: right;">
-								<h3 class="mt-20 mb-20">Exame MOS</h3>
-								<p class="date"><a href="#">16 jan 2019</a> <span class="lnr lnr-calendar-full"></span></p>
-							</div>
-						</div>
-						
-							
-						<div class="col-12">
-							<p class="excert-dentro">
-								Este exame pode ser feito por qualquer aluno da EPB - Escola Profissional de Braga sem qualquer custo, podendo ser fornecido uma certificação em MOS (Microsoft Office Specialist).
-							</p>
-							<p>
-								Esta certificação apresenta diversas vantagens, nomeadamente o reconhecimento do sector de TI, e competências profissionais de elevado nível que irão facilitar a entrada no mercado de trabalho aquando do recrutamento das empresas.
-							</p>
-							<p>
-								As aulas preparatórias serão nos dias 22, 23, 24 e 29 de janeiro e 01, 05, 06 e 07 de fevereiro, das 14h00 às 17h00, Salas INF1 ou Sala Rumos.
-							</p>
-							<!--<div class="quotes">
-								MCSE boot camps have its supporters and its detractors. Some people do not understand why you should have to spend money
-								on boot camp when you can get the MCSE study materials yourself at a fraction of the camp price. However, who has
-								the willpower to actually sit through a self-imposed MCSE training.
-							</div>-->
-
-						<div class="meta-details">
-							<ul class="tags">
-								<li><a href="#">Eventos</a></li>
-								<li><a href="#">Microsoft Office</a></li>
-							</ul>
-							<div class="user-details row">
-								<ul class="social-links col-12">
-									<li data-href="https://clubemicrosoftepb.000webhostapp.com/blog-single-mos.php"><a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fclubemicrosoftepb.000webhostapp.com%2Fblog-single-mos.php%2F&amp;src=sdkpreparse" class="fb-xfbml-parse-ignore"><i class="fa fa-facebook"></i></a></li>
-									<li><a target="_blank" href="https://twitter.com/intent/tweet?text=https://clubemicrosoftepb.000webhostapp.com/blog-single-mos.php Este exame pode ser feito por qualquer aluno da EPB - Escola Profissional de Braga sem qualquer custo, podendo ser fornecido uma certificação em MOS (Microsoft Office Specialist)."><i class="fa fa-twitter"></i></a></li>
-									<li><a onclick="linkedin(event, 'https://clubemicrosoftepb.000webhostapp.com/blog-single-mos.php', 'Clube Microsoft');return false;"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
-									<li><a href="https://plus.google.com/share?url=https://clubemicrosoftepb.000webhostapp.com/blog-single-mos.php"><i class="fa fa-google-plus"></i></a></li>
-									<li><a target="_blank" href="whatsapp://send?text=Este exame pode ser feito por qualquer aluno da EPB - Escola Profissional de Braga sem qualquer custo, podendo ser fornecido uma certificação em MOS (Microsoft Office Specialist). &ndash; https://clubemicrosoftepb.000webhostapp.com/blog-single-mos.php" title="Acesse de seu smartphone para enviar por WhatsApp"><i class="fa fa-whatsapp"></i></a></li>
-								</ul>
-							</div>
-						</div>
-					</div>
-				</div>
-				</div>
-			</div>
-		</div>
-	</section>
-	<!-- End post-content Area -->
-
-	<?php  require_once "footer.php"; ?>
-
-<script>
-	linkedin = function (event, s, a){
-    event.preventDefault();
-    window.open('https://www.linkedin.com/cws/share?url=' +s+ '?name=' +a, 'newwindow', 'width=680, height=450');
-};
-</script>
-
-</body>
-
+   <head>
+      <?php  require_once "links.php"; ?>
+      <!-- Site Title -->
+      <title>Exame MOS - Evento</title>
+   </head>
+   <body>
+      <?php  require_once "menu.php"; ?>
+      <!-- Start Banner Area -->
+      <section class="banner-area relative">
+         <div class="container">
+            <div class="row d-flex align-items-center justify-content-center">
+               <div class="about-content col-lg-12">
+                  <h1 class="text-white">
+                     Exame MOS
+                  </h1>
+                  <p>Este exame pode ser feito por qualquer aluno da EPB - Escola Profissional de Braga sem qualquer custo, podendo ser fornecido uma certificação em MOS (Microsoft Office Specialist).</p>
+                  <div class="link-nav">
+                     <span class="box">
+                     <a href="index.php">Home </a>
+                     <i class="lnr lnr-arrow-right"></i>
+                     <a href="blog-home.php">Blog</a>
+                     <i class="lnr lnr-arrow-right"></i>
+                     <a href="blog-single-mos.php">Exame MOS</a>
+                     </span>
+                  </div>
+               </div>
+            </div>
+         </div>
+         <div class="rocket-img go-bottom">
+            <img src="img/rocket.png" alt="" style="margin-bottom:-8px;">
+         </div>
+      </section>
+      <!-- End Banner Area -->
+      <!-- Start post-content Area -->
+      <section class="post-content-area single-post-area">
+         <div class="container">
+            <div class="row">
+               <div class="col-lg-12 posts-list">
+                  <div class="single-post row">
+                     <div class="col-lg-12">
+                        <div class="feature-img col-12 col-lg-8" style="float: left;">
+                           <img class="img-fluid img-blog-post-dentro" src="img/blog/mos_blog.png" alt="">
+                        </div>
+                        <div class="col-12 col-lg-4" style="float: right;">
+                           <h3 class="mt-20 mb-20">Exame MOS</h3>
+                           <p class="date"><a href="#">16 jan 2019</a> <span class="lnr lnr-calendar-full"></span></p>
+                        </div>
+                     </div>
+                     <div class="col-12">
+                        <p class="excert-dentro">
+                           Este exame pode ser feito por qualquer aluno da EPB - Escola Profissional de Braga sem qualquer custo, podendo ser fornecido uma certificação em MOS (Microsoft Office Specialist).
+                        </p>
+                        <p>
+                           Esta certificação apresenta diversas vantagens, nomeadamente o reconhecimento do sector de TI, e competências profissionais de elevado nível que irão facilitar a entrada no mercado de trabalho aquando do recrutamento das empresas.
+                        </p>
+                        <p>
+                           As aulas preparatórias serão nos dias 22, 23, 24 e 29 de janeiro e 01, 05, 06 e 07 de fevereiro, das 14h00 às 17h00, Salas INF1 ou Sala Rumos.
+                        </p>
+                        <!--<div class="quotes">
+                           MCSE boot camps have its supporters and its detractors. Some people do not understand why you should have to spend money
+                           on boot camp when you can get the MCSE study materials yourself at a fraction of the camp price. However, who has
+                           the willpower to actually sit through a self-imposed MCSE training.
+                           </div>-->
+                        <div class="meta-details">
+                           <ul class="tags">
+                              <li><a href="#">Eventos</a></li>
+                              <li><a href="#">Microsoft Office</a></li>
+                           </ul>
+                           <div class="user-details row">
+                              <ul class="social-links col-12">
+                                 <li data-href="https://clubemicrosoftepb.000webhostapp.com/blog-single-mos.php"><a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fclubemicrosoftepb.000webhostapp.com%2Fblog-single-mos.php%2F&amp;src=sdkpreparse" class="fb-xfbml-parse-ignore"><i class="fa fa-facebook"></i></a></li>
+                                 <li><a target="_blank" href="https://twitter.com/intent/tweet?text=https://clubemicrosoftepb.000webhostapp.com/blog-single-mos.php Este exame pode ser feito por qualquer aluno da EPB - Escola Profissional de Braga sem qualquer custo, podendo ser fornecido uma certificação em MOS (Microsoft Office Specialist)."><i class="fa fa-twitter"></i></a></li>
+                                 <li><a onclick="linkedin(event, 'https://clubemicrosoftepb.000webhostapp.com/blog-single-mos.php', 'Clube Microsoft');return false;"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+                                 <li><a href="https://plus.google.com/share?url=https://clubemicrosoftepb.000webhostapp.com/blog-single-mos.php"><i class="fa fa-google-plus"></i></a></li>
+                                 <li><a target="_blank" href="whatsapp://send?text=Este exame pode ser feito por qualquer aluno da EPB - Escola Profissional de Braga sem qualquer custo, podendo ser fornecido uma certificação em MOS (Microsoft Office Specialist). &ndash; https://clubemicrosoftepb.000webhostapp.com/blog-single-mos.php" title="Acesse de seu smartphone para enviar por WhatsApp"><i class="fa fa-whatsapp"></i></a></li>
+                              </ul>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </section>
+      <!-- End post-content Area -->
+      <?php  require_once "footer.php"; ?>
+      <script>
+         linkedin = function (event, s, a){
+            event.preventDefault();
+            window.open('https://www.linkedin.com/cws/share?url=' +s+ '?name=' +a, 'newwindow', 'width=680, height=450');
+         };
+      </script>
+   </body>
 </html>
