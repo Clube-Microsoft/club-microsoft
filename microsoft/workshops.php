a8f96de17d4942f4f5633bc3460384e068358179
Ruisics <36693911+Ruisics@users.noreply.github.com>
2019-04-23 23:12:55 +0100

    Workshops

---
+++ microsoft/workshops.php
@@ -0,0 +1,169 @@
+
+	<?php  require_once "links.php"; ?>
+	<!-- Site Title -->
+	<title>Contacto</title>
+    <script src="jsx/sweetalert.js"></script>
+	<link rel="stylesheet" href="css/form-style.css">
+</head>
+
+<body>
+	<?php  
+		require_once "menu.php";
+
+		//Define todas as variáveis iniciais
+		$nomeErr = $emailErr = "";
+		$nome = $email = "";
+
+		//Verifica se o botão do formulário é pressionado
+		if ($_SERVER["REQUEST_METHOD"] 	== "POST") {
+			
+			//Verifica se o campo "nome" está vazio
+			if (empty($_POST["nome"])) {
+				$nomeErr = "Preencha o nome";
+			} else {
+				//Lê o que foi escrito no campo "nome"
+				$nome = test_input($_POST["nome"]);
+				//Verifica se o nome não contém números ou caracteres especiais
+				if (!preg_match("/^\p{L}([- ']\p{L}|\p{L})*$/u", $nome)) {
+					$nomeErr = "Só são permitidas letras e espaços em branco";
+				}
+			}
+
+			//Verifica se o campo "email" está vazio
+			if (empty($_POST["email"])) {
+				$emailErr = "Preencha o email";
+			} else {
+				//Lê o que foi escrito no campo "email"
+				$email = test_input($_POST["email"]);
+				//Verifica se o email está bem formatado
+				if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+					$emailErr = "Formato de email inválido";
+				}
+			}
+			
+			//Verifica se nenhum erro foi ativado
+			if ($nomeErr == "" && 
+			$emailErr == "" && 
+			$generoErr == "" && 
+			$websiteErr == "" && 
+			$passwordErr == "" && 
+			$dateErr == "" && 
+			$imgErr == "") {
+				//Enviar email
+			}
+			
+		}
+
+		//Função para leitura dos dados introduzidos pelo utilizador
+		function test_input($dados) {
+			
+			$dados = trim($dados);
+			$dados = stripslashes($dados);
+			$dados = htmlspecialchars($dados);
+			return $dados;
+			
+		}	
+
+		?>
+    <!-- Start Banner Area -->
+	<section class="banner-area relative">
+		<div class="container">
+			<div class="row d-flex align-items-center justify-content-center">
+				<div class="about-content col-lg-12">
+					<h1 class="text-white">
+							Workshops
+					</h1>
+					<p>
+						Envia-nos a tua ideia.
+					</p>
+					<div class="link-nav">
+						<span class="box">
+							<a href="index">Início</a>
+							<i class="lnr lnr-arrow-right"></i>
+							<a href="workshops">Workshops</a>
+						</span>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="rocket-img go-bottom">
+			<img src="img/rocket.png" alt="" style="margin-bottom:-8px;">
+		</div>
+	</section>
+	<!-- End Banner Area -->
+
+	<!-- Start contact-page Area -->
+	<section class="contact-page-area section-gap">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-4">
+					<h1>Qual a tua ideia?</h1>
+					<p>Descreve-nos a tua ideia e entraremos em contacto.</p>
+				</div>
+				<div class="col-lg-8 form-style-5">
+					<form action="" method="post" enctype="multipart/form-data" class="form form-area">
+						<fieldset>
+							<legend>
+								<span class="number">
+									1
+								</span> 
+								Informação Pessoal
+							</legend>
+							<!-- Inputs -->
+							<label>Nome: <span class="error">* <?php echo $nomeErr;?></span></label>
+							<input placeholder="Introduza o seu nome..." type="text" name="nome" required>
+							<br/><br/>
+							<label>Email: <span class="error">* <?php echo $emailErr;?></span></label>
+							<input placeholder="Introduza o seu email..." type="email" name="email" required>
+							<br/><br/>
+							<label>Tema: <span class="error">*</span></label>
+							<input placeholder="Define o tema..." type="text" name="tema" required>
+							<br/><br/>
+							<label>Descrição: <span class="error">*</span></label>
+							<textarea name="desc" rows="5" cols="40" required></textarea>
+							<br/><br/>
+							<legend>
+								<span class="number">
+									2
+								</span> 
+								Informação Adicional
+							</legend>
+							<label>Duração</label>
+							<input type="number" name="duracao">	minutos
+							<br/><br/>
+							<label>Observações:</label>
+							<textarea name="obs" rows="5" cols="40"></textarea>
+							<br/><br/>
+							<span class="error">* campo obrigatório</span>
+						</fieldset>
+						<input type="submit" name="submit" value="Enviar"><br/>
+					</form>
+					<div class="mostrar"></div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- End contact-page Area -->
+
+	<?php  require_once "footer.php"; ?>
+	
+<script>
+	$(function(){
+		$('.form').submit(function(){
+			$('.loading').html("<img src='loading.gif' width='45'>");
+			$.ajax({
+				url: 'send-form.php',
+				type: 'POST',
+				data: $('.form').serialize(),
+				success: function(data){
+					$('.mostrar').html(data);
+					$('.loading').hide();
+					$('.form')[0].reset();
+				}
+			});
+			return false;
+		});
+	});
+</script>
+</body>
+</html>
